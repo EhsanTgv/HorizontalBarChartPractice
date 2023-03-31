8d3d1410ab49ada21e5a9a81afd9b977edb2a41f
EhsanTgv <EhsanTgv@gmail.com>
2023-03-31 11:00:46 +0300

    Initial Commit

---
+++ HorizontalBarChartPractice/HorizontalBarChartPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalBarChartPracticeApp.swift
+//  HorizontalBarChartPractice
+//
+//  Created by Ehsan Taghavi on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalBarChartPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
